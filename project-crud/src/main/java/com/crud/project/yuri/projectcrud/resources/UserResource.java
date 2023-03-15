d5486135af26731419a50585d8b86be351570663
Pistoled999 <yuri_matteus@hotmail.com>
2023-03-15 00:09:31 -0300

    User insert

--- project-crud/src/main/java/com/crud/project/yuri/projectcrud/resources/UserResource.java
+++ project-crud/src/main/java/com/crud/project/yuri/projectcrud/resources/UserResource.java
@@ -4,7 +4,9 @@ import com.crud.project.yuri.projectcrud.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,5 +24,13 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
